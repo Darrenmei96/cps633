6f05aa5d3969aa61cc980a737899e95d853a3fec
Darren Mei <darrenmei@hotmail.com>
2016-09-29 22:04:48 -0400

    Adding incomplete code

--- olmhash.c
+++ olmhash.c
@@ -1,3 +1,30 @@
 /*
 	OLMHash Password Scheme
 */
+
+struct userhash{
+	char name[32];
+	char hash[32];
+}
+
+typedef struct userhash userhash
+
+int prompt(char ){
+	/*
+		Get the userID
+	*/
+	int userIDLength = 32;
+	char userID[userIDLength];
+	printf("Enter in your user ID: ");
+	fgets(userID, userIDLength - 1, stdin);
+
+	int i = 1, max_attempts = 3;
+	char pass[12];
+	do{
+		printf("Enter in your password :");
+		fgets(pass, 11, stdin);
+
+
+	}while (i < max_attempts);
+
+}
